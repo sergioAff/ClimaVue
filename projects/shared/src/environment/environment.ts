f2a9fa7894ae493a323de594443e83245f9a07e6
Sergio Adrian Fernández <118078644+sergioAff@users.noreply.github.com>
2025-03-28 19:55:38 -0300

    Delete projects/shared/src/environment/environment.ts

--- projects/shared/src/environment/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  apiKeyWeather: 'qytivcd66ldmyvby9jswcnzr8l0y3oq8uqzd24u8',
-  apiKeyWallpaper: '55zfUFQ2WF1NCXtKRjufbA6vnxSgNridiHn1j2bosdECAUv3HfUS5h5f',
-  apiWeatherGetPlacesPrefix:
-    'https://www.meteosource.com/api/v1/free/find_places_prefix?text=',
-  apiWeather: 'https://www.meteosource.com/api/v1/free/point?place_id=',
-};
